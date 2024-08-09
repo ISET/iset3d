a7e9d442cf9f03273f9a3a26f75117995d4f207e
Brian Wandell <1837145+wandell@users.noreply.github.com>
2024-08-09 11:33:58 -0700

    comments

--- scripts/database/s_database.m
+++ scripts/database/s_database.m
@@ -11,30 +11,93 @@ piDockerConfig;
 % docker for rendering scenes 
 thisDocker = isetdocker();
 
+% Always this weird number for Stanford and acorn.
+% The database is on acorn and accessible via that port.
 setpref('db','port',49153);
 ourDB = isetdb;
 
 % if ~isopen(ourDB.connection),error('No connection to database.');end
+
+% Zhenyi uses this one.  Maybe we will rename to iset3d-tiny or iset3d
+% or something.
 collectionName = 'PBRTResources'; % ourDB.collectionCreate(colName);
 
 %% Render local scene with remote PBRT
+
 % Make sure you have configured your computer according to this:
+%
 %       https://github.com/ISET/iset3d/wiki/Remote-Rendering-with-PBRT-v4
+%
 % See /iset3d/tutorials/remote/s_remoteSet.m for remote server
 % configuration
 
 % getpref('ISETDocker') % set up by isetdocker.setUserPrefs()
 
+% The local folder can can contain any PBRT scene
 localFolder = '/Users/zhenyi/git_repo/dev/iset3d/data/scenes/slantedEdge';
-% localFolder = '/Users/wandell/Documents/MATLAB/iset3d-v4/data/scenes/materialball';
+% localFolder = '/Users/wandell/Documents/MATLAB/iset3d-v4/data/scenes/slantedEdge';
 
 pbrtFile = fullfile(localFolder, 'slantedEdge.pbrt');
 thisR = piRead(pbrtFile);
-thisR.set('spatial resolution',[100,100]);
-piWrite(thisR);
 
+% Edit for a while.
+thisR.set('spatial resolution',[100,100]);
+
+% This must exist on your path.  It will be copied locally and then
+% sync'd to the remote machine.
+thisR.set('skymap','room.exr');
+
+% This writes to iset3d-tiny/local
+piWrite(thisR);  
 scene = piRender(thisR,'docker',thisDocker);
+
 sceneWindow(scene);
+
+%% Render a scene from data base
+
+% We expect people to include a recipe.mat file in the database.  We
+% do handle the case in which there is no recipe file.
+sceneName       = 'ChessSet';
+
+% Returns a struct from the database defining properties of the scene.
+thisScene = ourDB.contentFind('PBRTResources', 'name',sceneName, 'show',true);
+
+% The scenes in this database have a recipe.mat.  The input file is on
+% acorn.  The docker has the information about the host, username, and
+% directories to find the recipe.
+recipeDB = piRead(thisScene,'docker',thisDocker);
+
+recipeDB.set('spatial resolution',[512,512]);
+recipeDB.set('lights','all','delete');
+
+% A local skymap, which will be uploaded to the user's remote
+% directory.  (The path needs to be fixed).
+recipeDB.set('skymap','room.exr');
+
+% This writes the recipe.  We need to understand why the textures are
+% being written locally when we are using remote rendering.  Shouldn't
+% happen.
+piWrite(recipeDB);
+
+%
+scene = piRender(recipeDB,'docker',thisDocker);
+sceneWindow(scene);
+
+
+%% Change the skymap to a skymap in the database
+
+%
+remoteSkymaps = ourDB.contentFind('PBRTResources','type','skymap', 'show',true);
+
+% need a function to remove skymap using type rather than name.
+recipeDB.set('light','all','delete');
+
+recipeDB.set('skymap',remoteSkymaps(1));
+
+scene = piWRS(recipeDB);
+
+%% Use a texture in the database
+
 
 %% Add a scene to the database, and render it remotely
 
@@ -64,35 +127,6 @@ ourDB.contentCreate('collection Name',collectionName, ...
 thisDocker.upload(localFolder,remoteDBDir);
 % remove the mat file from local folder
 delete(recipeMATFile);
-
-%% Render a scene from data base
-
-% Find it
-thisScene = ourDB.contentFind('PBRTResources', 'name',sceneName, 'show',true);
-
-% Get recipe mat
-recipeDB = piRead(thisScene,'docker',thisDocker);
-% 
-recipeDB.set('spatial resolution',[127,127]);
-%
-piWrite(recipeDB);
-%
-scene = piRender(recipeDB,'docker',thisDocker);
-sceneWindow(scene);
-
-
-%% change the skymap to a skymap in the database
-remoteSkymaps = ourDB.contentFind('PBRTResources','type','skymap', 'show',true);
-
-% need a function to remove skymap using type rather than name.
-recipeDB.set('light','all','delete');
-
-recipeDB.set('skymap',remoteSkymaps(1));
-
-scene = piWRS(recipeDB);
-
-%% Use a texture in the database
-
 
 %% END
 
