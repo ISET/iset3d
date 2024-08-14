7d5e19fd4fde90f56850c2c519a6b3eaf5ac3f4c
Zhenyi Liu <zhenyi27@stanford.edu>
2024-08-14 11:52:36 -0700

    Works for now, more examples to test

--- scripts/database/s_dbRecipeMerge.m
+++ scripts/database/s_dbRecipeMerge.m
@@ -5,13 +5,13 @@ piDockerConfig;
 
 %%
 thisDocker = isetdocker();
-%% Local cornell box
+%% Local cornell box + local bunny
 parentRecipe = piRecipeDefault('scene name','cornell_box');
 lightName = 'from camera';
 ourLight = piLightCreate(lightName,...
     'type','distant');
 recipeSet(parentRecipe,'lights', ourLight,'add');
-%% Add a local bunny to cornell box
+% load a local bunny
 assetName = which('bunny.mat');
 ourAsset  = piAssetLoad(assetName);
 
@@ -26,9 +26,9 @@ combinedR = piRecipeMerge(parentRecipe, ourAsset.thisR);
 combinedR.show('textures');
 
 % Render it
-result = piWRS(combinedR);
+result = piWRS(combinedR,'name','local cornell and local bunny');
 
-%%
+%% Local cornell box + database bunny
 parentRecipe = piRecipeDefault('scene name','cornell_box');
 lightName = 'from camera';
 ourLight = piLightCreate(lightName,...
@@ -45,4 +45,23 @@ bunnyR.set('asset',4,'scale',[2 2 2]);
 bunnyR.set('asset',4,'scale',[0.1 0.1 0.1] ./ sz);
 bunnyR.set('asset',4,'translation',[0 0 0.5000]);
 combinedR_2 = piRecipeMerge(parentRecipe, bunnyR);
-result = piWRS(combinedR_2);
+result = piWRS(combinedR_2,'name','local cornell and database bunny');
+%% Database cornell box + database bunny
+db_cbox = pbrtDB.contentFind('PBRTResources', 'name','cornell_box', 'show',true);
+parentRecipe = piRead(db_cbox,'docker',thisDocker);
+lightName = 'from camera';
+ourLight = piLightCreate(lightName,...
+    'type','distant');
+recipeSet(parentRecipe,'lights', ourLight,'add');
+% Add a database bunny
+pbrtDB = isetdb;
+db_bunny = pbrtDB.contentFind('PBRTResources', 'name','bunny', 'show',true);
+
+bunnyR = piRead(db_bunny,'docker',thisDocker);
+thisID = bunnyR.get('objects');   % Object id
+bunnyR.set('asset',4,'scale',[2 2 2]);
+
+bunnyR.set('asset',4,'scale',[0.1 0.1 0.1] ./ sz);
+bunnyR.set('asset',4,'translation',[0 0 0.5000]);
+combinedR_3 = piRecipeMerge(parentRecipe, bunnyR);
+result = piWRS(combinedR_3,'name','database cornell and database bunny');
