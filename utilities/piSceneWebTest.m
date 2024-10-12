570df0962b49614b9e96377ffc34faabe794369b
Brian Wandell <1837145+wandell@users.noreply.github.com>
2024-10-11 17:00:12 -0700

    Comments and proper download directory.

--- utilities/piSceneWebTest.m
+++ utilities/piSceneWebTest.m
@@ -4,6 +4,8 @@ function [sceneDir, zipfilenames] = piSceneWebTest(sceneName,sceneFile)
 % Synopsis
 %  [sceneDir, zipfilenames] = piSceneWebTest(sceneName,sceneFile)
 %
+% The scene is downloaded into fullfile(piRootPath,'data','scenes','web')
+%
 % Input
 %   sceneName
 %   sceneFile
@@ -12,18 +14,15 @@ function [sceneDir, zipfilenames] = piSceneWebTest(sceneName,sceneFile)
 %   N/A
 %
 % Output
-%  sceneDir - Name of the unzipped directory from SDR
-%  zipfilenames - Cell array of zip files in the directory
+%  sceneDir     - Name of the directory of the downloaded scene
+%  zipfilenames - Cell array of files in the directory
 %
 % Description
-%  The sceneName should correspond to one of the lowercase.zip files in the
-%  ISET3D Scenes deposit on SDR.  There may be different acceptable
-%  sceneFile strings for a sceneName.  For example, there are several
-%  legitimate sceneFile names for bistro, dambreak, and others.
-%
-%  There is a scene called 'head' in both iset3d-scene and pbrtv4. By
-%  default head gets the iset3d version, and head-pbrt gets the pbrtv4
-%  version.
+%  The sceneName should correspond to one of the lowercase.zip files
+%  in the ISET3D Scenes deposit on SDR.  There may be different
+%  acceptable sceneFile strings for a sceneName.  For example, there
+%  are several legitimate sceneFile names for bistro, dambreak, and
+%  others.
 %
 %  For bistro, we have sceneName as bistro and different sceneFiles, such
 %  as bistro_vespa
@@ -39,11 +38,14 @@ sceneFile = fullfile(sceneDir,sceneFile);
 % Download the file to data/scene/web
 if ~isfolder(sceneDir)
     depositName = piSceneDeposit(sceneName);
-    [sceneDir, zipfilenames] = ieWebGet('deposit name', depositName, 'deposit file', [sceneName,'.zip'],  'unzip', true);
+    [sceneDir, zipfilenames] = ieWebGet('deposit name', depositName, ...
+        'deposit file', [sceneName,'.zip'],  ...
+        'download dir', fullfile(piRootPath,'data','scenes','web'),...
+        'unzip', true);
 elseif ~exist(sceneFile,'file')
     error('Folder exists, but sceneFile (%s) is not there.\n',sceneFile);
 else
-    fprintf('File %s already present in data/scene/web.\n',sceneName)
+    fprintf('File %s already present in %s.\n',sceneName,sceneDIr)
 end
 
 end
