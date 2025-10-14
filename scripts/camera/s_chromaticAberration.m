f610c7995a2100e8319a216aa5a46d92f6566ee6
Brian Wandell <1837145+wandell@users.noreply.github.com>
2025-10-14 11:27:54 -0700

    Refactor denoising and window display calls
    Removed 'batch' and 'replace' arguments from piAIdenoise and scene/oiWindow calls in t_piIntro_camera.m for simplification. Added comments and minor formatting improvements in s_chromaticAberration.m.

--- scripts/camera/s_chromaticAberration.m
+++ scripts/camera/s_chromaticAberration.m
@@ -20,13 +20,16 @@ pointLight = piLightCreate('point','type','point','cameracoordinate', true);
 thisR.set('light',pointLight, 'add');
 
 %% Attach a desired texture to part of the scene
+
+% Figure out how to use the search to get the asset that contains the
+% string 'large_box"
 ourAsset = 'large_box_O';
+
 piMaterialsInsert(thisR,'names','slantededge');
 piAssetTranslate(thisR,ourAsset,[.15 .11 0]);
 thisR.set('asset',ourAsset,'material name','slantededge');
 
 piWRS(thisR,'name','pinhole');
-
 
 %% Attach a camera with a lens
 
