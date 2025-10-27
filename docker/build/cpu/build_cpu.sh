d35447fd24e409ddbae942eb14f69db7046cd56f
dawfedora <dawfedora@gmail.com>
2025-10-27 11:35:10 -0700

    Update cpu build

--- docker/build/cpu/build_cpu.sh
+++ docker/build/cpu/build_cpu.sh
@@ -1,5 +1,9 @@
-#docker build --no-cache --tag camerasimulation/pbrt-v4-cpu `pwd`
-docker build -f Dockerfile_cpu --tag digitalprodev/pbrt-v4-cpu `pwd`
-docker push digitalprodev/pbrt-v4-cpu:latest
+#/bin/sh
+#
+ARCH=`arch`
+ARCH=linux/arm64/v8
+TAG=arm
+docker buildx build --platform=$ARCH -f Dockerfile_cpu --tag vistalab/pbrt-v4-cpu:$TAG  `pwd`
+#docker push vistalab/pbrt-v4-cpu:$TAG
 
 
