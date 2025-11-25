603211936ef37cba3a0f141b062133ff504f7ad1
dawfedora <dawfedora@gmail.com>
2025-11-25 11:37:26 -0800

    cleaning up gpu build

--- docker/build/gpu/build_gpu.sh
+++ docker/build/gpu/build_gpu.sh
@@ -3,8 +3,12 @@
 #
 # make sure you have docker runtime set as nvidia-runtime
 # docker build --no-cache --tag camerasimulation/pbrt-v4-gpu `pwd`
-docker build -f Dockerfile_volta --tag digitalprodev/pbrt-v4-gpu-volta-mux `pwd`
+TAG=`arch`
+ARCH=linux/$TAG
+docker buildx build --platform=$ARCH -f Dockerfile_ampere --tag vistalab/pbrt-v4-gpu:$TAG `pwd`
+
 
 # if you have permission to push, and know what you are doing...
-docker push digitalprodev/pbrt-v4-gpu-volta-mux:latest
+#docker push digitalprodev/pbrt-v4-gpu-ampere
+#docker tag digitalprodev/pbrt-v4-gpu-ampere digitalprodev/pbrt-v4-gpu-ampere
 
